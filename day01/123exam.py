f28470d1fcdbd4f225085fd83ef8229ba02a1b2a
PARK MIYEON <miyoun1003@naver.com>
2023-08-22 20:22:43 +0900

    230822 369 짝 뽀숑 출력 성공. 소수 찾기 진행중....

---
+++ day01/123exam.py
@@ -0,0 +1,6 @@
+data = input('데이터 입력 : ').split()
+
+print(type(data), data)
+
+for item in data:
+    print(type(item))
--- day01/369exam.py
+++ day01/369exam.py
@@ -6,10 +6,19 @@ print(r)
 
 
 for i in range(r):
-    if (i+1) % 10:
-        print(i + 1)
+    bbo = (i+1) // 10
+    jja = (i+1) % 10
+    if (i+1) % 10 == 0:
+        if bbo == 3 or bbo == 6 or bbo == 9:
+            print('짝',bbo * '뽀', '숑', sep='')
+        else:
+            print(bbo * '뽀', '숑', sep='')
+    elif bbo == 3 or bbo == 6 or bbo == 9:
+        if jja == 3 or jja == 6 or jja == 9:
+            print('짝짝')
+        else:
+            print('짝')
+    elif jja == 3 or jja == 6 or jja == 9:
+            print('짝')
     else:
-        print('뽀숑')
-
-
-print(list(range(3, 10, 3)))
+        print(i + 1)
